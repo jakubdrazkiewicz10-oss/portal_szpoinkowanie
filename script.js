8ef4a09697b270fc68a372b29b502aca8c81bef1
jakubdrazkiewicz10-oss <jakubdrazkiewicz10@gmail.com>
2026-09-05 18:57:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,6 @@ function showMusicFolder() {
     addressPath.textContent = "Mój Komputer > Szpoinkowanie > Muzyka";
     if (backBtn) {
         backBtn.style.display = "inline-block";
-        // Specjalna obsługa wyświetlania przycisku wstecz na telefonach
         if (window.innerWidth <= 768) {
             backBtn.style.display = "block";
             backBtn.style.marginBottom = "10px";
